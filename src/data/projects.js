5a5ec5e902572a678f784cd0bda236952c20836c
Abel Acosta <abel.2833@hotmail.com>
2026-05-31 21:12:56 -0500

    feat(projects): add projects showcase section

---
+++ src/data/projects.js
@@ -0,0 +1,24 @@
+import bellasArtes from "../assets/images/projects/BellasArtes.webp";
+import artesanias from "../assets/images/projects/ArtesaniasRinoceronte.webp";
+import manualidades from "../assets/images/projects/Manualidades.webp";
+
+export const projects = [
+	{
+		title: "Proyecto Bellas Artes",
+		description: "Tienda Online realizada en WordPress.",
+		techStack: ["WordPress", "Elementor"],
+		image: bellasArtes,
+	},
+	{
+		title: "Proyecto Artesanias",
+		description: "Página informativa sobre los productos que ofrece el negocio.",
+		techStack: ["HTML", "CSS", "JavaScript"],
+		image: artesanias,
+	},
+	{
+		title: "Manualidades",
+		description: "Pagina informativa de negocio de manualidades.",
+		techStack: ["HTML", "CSS", "JavaScript", "Bootstrap"],
+		image: manualidades,
+	},
+];
